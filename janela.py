d63ed4f582b2a162ece4c68b1b4149a924793d1b
Kamille <kamille.kpimentel@gmail.com>
2024-11-19 19:52:21 -0300

    first commit front

---
+++ janela.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+corBackground = "#A7C5BD"
+corBackgroundMeio = "#F7F0E1"
+corTexto = "#2F2831"
+corTextoInativo = "#A8A6A8"
+corSetasBordas = "#C6C5C5"
+corBotoesInputs = "#F3F3F3"
+corOpcoes = "#FFFFFF"
+
+janela = Tk()
+janela.geometry("1280x720")
+janela.title("Sistema para pesquisas políticas")
+janela.configure(bg=corBackground)
+
+janela.mainloop()
